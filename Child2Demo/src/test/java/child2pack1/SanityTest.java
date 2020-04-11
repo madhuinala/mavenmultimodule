ad9aa4aa48ab063b697fde9fba83aec2f0004626
Madhu Babu Inala <madhu.inala@fox.com>
2020-04-11 19:58:38 +0530

    added smoke and sanity tests

---
+++ Child2Demo/src/test/java/child2pack1/SanityTest.java
@@ -0,0 +1,21 @@
+package child2pack1;
+
+import org.testng.annotations.Test;
+
+public class SanityTest {
+
+	@Test
+	public void sanitythod1(){
+		System.out.println("This is sanity test method1");
+	}
+	
+	@Test
+	public void sanitymethod2(){
+		System.out.println("This is sanity test method2");
+	}
+	
+	@Test
+	public void sanitymethod3(){
+		System.out.println("This is sanity test method3");
+	}
+}
---
+++ Child2Demo/src/test/java/child2pack1/SmokeTest.java
@@ -0,0 +1,23 @@
+package child2pack1;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+	
+	@Test
+	public void smokemethod1(){
+		System.out.println("This is smoke test method1");
+	}
+	
+	@Test
+	public void smokemethod2(){
+		System.out.println("This is smoke test method2");
+	}
+	
+	@Test
+	public void smokemethod3(){
+		System.out.println("This is smoke test method3");
+	}
+	
+}
